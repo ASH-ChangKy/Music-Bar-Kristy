1f6538055f72b2d18114ddac2a2c92f393c6bd5d
763673994@qq.com <763673994@qq.com>
2025-02-18 21:27:59 +0900

    123

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HashRouter>
+          <nav>
+              <Link to="/">主页</Link>
+              <Link to="/about">关于我们</Link>
+              <Link to="/menu">菜单</Link>
+          </nav>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/menu" element={<Menu />} />
+          </Routes>
+      </HashRouter>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+    return <h1>关于我们</h1>;
+}
+
+export default About;
